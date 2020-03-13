f3108e71dd18c93161eb8ce539df23a45d17151a
Michael Harris <mhguitar1997@gmail.com>
2020-03-13 08:34:32 -0700

    oop2 test working

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -9,7 +9,7 @@ class GroundVehicle():
 
     # TODO
     def drive(self):
-        return "vrooom"
+        return "vroooom"
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -39,3 +39,5 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+for vehicle in vehicles:
+    print(vehicle.drive())
